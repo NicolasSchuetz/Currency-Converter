babc396511b46af0704ccf2ffd236384efa2b75d
NicolasSchuetz <nic.schuetz08@gmail.com>
2025-02-28 11:27:33 +0100

    Easy Multi Language support added

--- main.py
+++ main.py
@@ -1,17 +1,58 @@
 #Imports
-import tkinter as tk 
+import tkinter as tk
 
+#Multi language Support
+translations={
+    "en":{"title":"Currency Convertor",
+          "button":"Convert",},
+    "de":{"title":"Währungsrechner",
+          "button":"Umrechnen",},
+    "fr":{"title":"Convertisseur de devises",
+          "button":"Convertir",},
+    "es":{"title":"Conversor de monedas",
+          "button":"Convertir",},
+    "it":{"title":"Convertitore di valute",
+          "button":"Convertire",},
+    "pt":{"title":"Conversor de moedas",
+          "button":"Converter",},
+    "ru":{"title":"Конвертер валют",
+          "button":"Преобразовать",},} 
+language="ru"
+title="Currency Convertor"
+button="Convert"
+if language =="de":
+    title=translations["de"]["title"]
+    button=translations["de"]["button"]
+elif language =="en":
+    title=translations["en"]["title"]
+    button=translations["en"]["button"]
+elif language =="fr":
+    title=translations["fr"]["title"]
+    button=translations["fr"]["button"]
+elif language =="es":
+    title=translations["es"]["title"]
+    button=translations["es"]["button"]
+elif language =="it":
+    title=translations["it"]["title"]
+    button=translations["it"]["button"]
+elif language =="pt":
+    title=translations["pt"]["title"]
+    button=translations["pt"]["button"]
+elif language =="ru":
+    title=translations["ru"]["title"]
+    button=translations["ru"]["button"]
 
+#Class
 class CurrencyConverter:
     def __init__(self,root):
         #Result variable
         self.result = tk.StringVar()
         #Window
         self.root = root
-        self.root.title("Currency Converter")
+        self.root.title(title)
         self.root.geometry("400x300")
         #Convert Button
-        self.button=tk.Button(root,text="Convert",command=self.convert)
+        self.button=tk.Button(root,text=button,command=self.convert)
         self.button.grid(row=0,column=1)
         #Textfeldeingabe
         self.curr1=tk.Entry(root)
